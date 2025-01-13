33c66a920953453935c9fa68de206eb87417f3e2
Kalel-Marquez <34970@ocvts.org>
2025-01-13 17:51:32 +0000

    better table, card deck

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 let config ={
     type: Phaser.AUTO,
-    width: 800,
+    width: 900,
     height: 600,
+    backgroundColor: '#17041f',
     physics: {
         default: 'arcade',
         arcade: {
@@ -19,14 +20,25 @@ let config ={
 var game = new Phaser.Game(config);
 
 function preload () {
-    this.load.image('background', 'assets/1.png');
-//     this.load.image('table', 'assets/1.png');
+    // this.load.image('background', 'assets/7.png');
+    this.load.image('table', 'assets/Tablex2.png');
+    this.load.spritesheet('cards', 'assets/cards-sheet.png',
+        {frameWidth: 32, frameHeight: 47}
+    )
 }
 
 function create () {
-    // this.add.image(400, 300, 'table').refreshBody();
-    this.add.image(400, 300, 'background').setScale(1.05, 1.08).refreshBody();
-
+    // this.add.image(450, 300, 'background').setScale(1.3, 1.08)
+    this.add.image(450, 300, 'table').setScale(1.5);
     
     
+    cards = this.add.group({
+        key: 'cards',
+        repeat: 51,
+        setXY: {x: 750, y: 300, stepX: -0.25, stepY: -0.25}
+    });
+    
+    cards.children.iterate(function (child) {
+        child.setScale(1.5);
+    })
 }
